35c2184d227a5af6de7392e1574c02e0376ad6d5
Lindsey <linds.fonnes@gmail.com>
2020-10-20 21:54:11 -0600

    task 1 finished

--- index.js
+++ index.js
@@ -567,25 +567,29 @@ const airports = [
 // console.log("Function was invoked!");
 // };
 // myFunction();
-
+myFunction = () => console.log("Function was invoked!");
+myFunction();
 
 // let anotherFunction = function (param) {
 //   return param;
 // };
 // anotherFunction("Example");
-
+anotherFunction = (param) => param;
+console.log(anotherFunction("Example"));
 
 // let add = function (param1, param2) {
 //   return param1 + param2;
 // };
 // add(1,2);
+add = (param1, param2) => param1 + param2;
 
-
+console.log(add(1,2));
 // let subtract = function (param1, param2) {
 //   return param1 - param2;
 // };
 // subtract(1,2);
-
+subtract = (param1, param2) => param1 - param2;
+console.log(subtract(1,2));
 
 /* Work out 💪 */
 /* TASK 1 🚀 
